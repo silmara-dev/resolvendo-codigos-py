b50771f0e38b6e44c389fa00cadb720e2b2571c3
Silmara <silmara.aocsilva@gmail.com>
2025-12-30 19:32:49 -0300

    Create verifica_num.py

---
+++ resolucoes_code/verifica_num.py
@@ -0,0 +1,9 @@
+#Como entrada, receba um número inteiro e verifique se ele é par ou ímpar. 
+# Uma dica é: Utilize condicionais para realizar a verificação.
+
+numero = int(input("Digite um número inteiro: "))
+
+if numero % 2 == 0
+    print("Par")
+    else
+        print("Ímpar")
