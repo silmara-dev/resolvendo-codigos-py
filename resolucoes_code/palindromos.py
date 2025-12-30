8162372e7982cc2fa62f1c24a2977c1a14589038
Silmara <silmara.aocsilva@gmail.com>
2025-12-30 19:56:16 -0300

    Update palindromos.py

--- resolucoes_code/palindromos.py
+++ resolucoes_code/palindromos.py
@@ -1,1 +1,14 @@
+#Vamos testar se uma palavra é um palíndromo?! Uma dica é: Utilize conceitos de manipulação de strings para inverter a palavra e comparar com a original.
+
+# Solicitando a palavra do usuário
+palavra = input("Digite uma palavra: ")
+
+# Invertendo a string
+palavra_invertida = palavra[::-1]
+
+# Verificando se é palíndromo
+if palavra == palavra_invertida:
+    print("A palavra é um palíndromo.")
+else:
+    print("A palavra não é um palíndromo.")
 
