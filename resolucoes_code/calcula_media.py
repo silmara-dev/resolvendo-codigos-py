ab8a23d27b9a3397e93709f85322d78d713a69ca
Silmara <silmara.aocsilva@gmail.com>
2025-12-30 19:55:57 -0300

    Update and rename calcula_media to calcula_media.py

---
+++ resolucoes_code/calcula_media.py
@@ -0,0 +1,12 @@
+#Agora vamos calcular a média de três notas fornecidas na entrada do usuário. 
+
+# Solicitando as três notas do usuário
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+# Calculando a média
+media = (nota1 + nota2 + nota3) / 3
+
+# Exibindo o resultado
+print("A média das notas é:", media)
