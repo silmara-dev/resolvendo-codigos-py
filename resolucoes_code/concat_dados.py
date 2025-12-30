dea5beeb4b31ee46fbee1487fe46e24e5918927b
Silmara <silmara.aocsilva@gmail.com>
2025-12-30 18:28:02 -0300

    Criação do código
    Criação do código do primeiro desafio

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,12 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+# Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+dado1 = input ("Digite a primeira informação:")
+dado2 = input ("Digite a primeira informação:")
+
+# Concatenando os dados em uma única string
+dados_concatenados = dado1 + " " + dado2
+
+# Exibindo o resultado
+print("As informações concatenadas são: ", dados_concatenados)
