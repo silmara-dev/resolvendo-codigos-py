85a62e0c091e4bf11bbc61ad0185cf687862a31d
Silmara <silmara.aocsilva@gmail.com>
2025-12-30 19:21:53 -0300

    Criação de codigo
    Criação do código para repetir strings

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,12 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Agora vamos solicitar uma string e um número inteiro como entrada.
+# Depois teremos que retornar a string repetida o número de vezes informado
+
+# Solicitando os dados do usuário
+texto = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+# Repetindo a string
+resultado = (texto + " " )* numero
+
+# Exibindo o resultado
+print(resultado)
